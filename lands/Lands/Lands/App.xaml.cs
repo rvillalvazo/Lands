5bf739d58011d7aea7324254152c409622b5f1dc
rvillalvazo <59748238+rvillalvazo@users.noreply.github.com>
2020-01-11 16:35:01 -0600

    Create App.xaml.cs

---
+++ lands/Lands/Lands/App.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+using Lands.Views;
+
+namespace Lands
+{   
+	public partial class App : Application
+	{
+        #region Constructor
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new LoginPage());//.MainPage();
+        }
+        #endregion
+
+        #region Metodos
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        } 
+        #endregion
+    }
+}
