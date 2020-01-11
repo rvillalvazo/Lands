6f17d9c8394146aca70d608d7b656cb473225d79
rvillalvazo <59748238+rvillalvazo@users.noreply.github.com>
2020-01-11 16:35:14 -0600

    Create LoginViewModel.cs

---
+++ lands/Lands/Lands/ViewModels/LoginViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lands.ViewModels
+{
+    using System.Windows.Input;
+
+    public class LoginViewModel
+    {
+        #region Propiedades
+        public string Email
+        {
+            get;
+            set;
+        }
+        public string Password
+        {
+            get;
+            set;
+        }        
+        public bool IsRunning
+        {
+            get;
+            set;
+        }
+        public bool IsRemembered
+        {
+            get;
+            set;
+        }
+        #endregion
+
+        #region Constructor
+        public LoginViewModel()
+        {
+            this.IsRemembered = true;
+        }
+        #endregion
+
+        #region Comandos
+        public ICommand LoginCommand
+        {
+            get;
+            set;
+        }
+        #endregion
+    }
+}
