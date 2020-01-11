77825e19514a25408e5f5108bc98f8f9823396c8
rvillalvazo <59748238+rvillalvazo@users.noreply.github.com>
2020-01-11 16:35:16 -0600

    Create MainViewModel.cs

---
+++ lands/Lands/Lands/ViewModels/MainViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lands.ViewModels
+{
+    public class MainViewModel
+    {
+        #region ViewModels
+        public LoginViewModel Login
+        {
+            get;
+            set;
+        }
+        #endregion
+
+        #region Contructores
+        public MainViewModel()
+        {
+            this.Login = new LoginViewModel();
+        }
+        #endregion
+    }
+}
