884d6932574504f8063b593024561d5c916b604f
rvillalvazo <59748238+rvillalvazo@users.noreply.github.com>
2020-01-11 23:06:24 -0600

    Update ApiService.cs

--- lands/Lands/Lands/Services/ApiService.cs
+++ lands/Lands/Lands/Services/ApiService.cs
@@ -62,5 +62,43 @@
                 return null;
             }
         }
+
+        public async Task<Response> GetList<T>(string urlBase,string servicePrefix,string controller)
+        {
+            try
+            {
+                var client = new HttpClient();
+                client.BaseAddress = new Uri(urlBase);
+
+                var url = String.Format("{0}{1}",servicePrefix,controller);
+                var response = await client.GetAsync(url);
+                var result = await response.Content.ReadAsStringAsync();
+
+                if(!response.IsSuccessStatusCode)
+                {
+                    return new Response
+                    {
+                        IsSuccess=false,
+                        Message=result,
+                    };
+                }
+
+                var list = JsonConvert.DeserializeObject<List<T>>(result);
+                return new Response
+                {
+                    IsSuccess=true,
+                    Message="OK",
+                    Result=list
+                };
+            }
+            catch(Exception ex)
+            {
+                return new Response
+                {
+                    IsSuccess=false,
+                    Message=ex.Message,
+                };
+            }
+        }
     }
 }
