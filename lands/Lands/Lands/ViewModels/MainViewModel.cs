fe68df515dd14b1bacd8ba49643fe7a40917fd3b
rvillalvazo <59748238+rvillalvazo@users.noreply.github.com>
2020-01-11 21:03:57 -0600

    Update MainViewModel.cs

--- lands/Lands/Lands/ViewModels/MainViewModel.cs
+++ lands/Lands/Lands/ViewModels/MainViewModel.cs
@@ -12,12 +12,30 @@ namespace Lands.ViewModels
             get;
             set;
         }
+        public LandsViewModel Lands        
+        {
+            get;
+            set;
+        }
         #endregion
 
         #region Contructores
         public MainViewModel()
         {
+            instance = this;
             this.Login = new LoginViewModel();
+        }
+        #endregion
+
+        #region Singleton
+        private static MainViewModel instance;
+
+        public static MainViewModel GetInstance()
+        {
+            if (instance == null)
+                instance = new MainViewModel();
+
+            return instance;
         }
         #endregion
     }
