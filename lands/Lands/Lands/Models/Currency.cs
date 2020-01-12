f0467b815ac53661bbe52dea34e060cf2c325bfd
rvillalvazo <59748238+rvillalvazo@users.noreply.github.com>
2020-01-11 21:03:45 -0600

    Create Currency.cs

---
+++ lands/Lands/Lands/Models/Currency.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lands.Models
+{
+    using Newtonsoft.Json;
+
+    public class Currency
+    {
+        #region Propiedades
+        [JsonProperty (PropertyName ="code")]
+        public string Code { get; set; }
+
+        [JsonProperty(PropertyName = "name")]
+        public string Name { get; set; }
+
+        [JsonProperty(PropertyName = "symbol")]
+        public string Symbol { get; set; }
+        #endregion
+    }
+}
