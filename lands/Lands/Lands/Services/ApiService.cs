d6ad33bfee5a7a5cdafe6d406f366512bd6eb116
rvillalvazo <59748238+rvillalvazo@users.noreply.github.com>
2020-01-11 21:57:27 -0600

    Create ApiService.cs

---
+++ lands/Lands/Lands/Services/ApiService.cs
@@ -0,0 +1,66 @@
+﻿namespace Lands.Services
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using System.Net.Http;
+    using System.Net.Http.Headers;
+    using System.Threading.Tasks;
+    using Models;
+    using Newtonsoft.Json;
+    using Plugin.Connectivity;
+
+    public class ApiService
+    {
+        public async Task<Response> CheckConnection()
+        {
+            if(!CrossConnectivity.Current.IsConnected)
+            {
+                return new Response
+                {
+                    IsSuccess = false,
+                    Message = "Please turn on your internet settings.",
+                };
+            }
+
+            var isReachable = await CrossConnectivity.Current.IsRemoteReachable("google.com");
+            if(!isReachable)
+            {
+                return new Response
+                {
+                    IsSuccess=false,
+                    Message="Check your internet connection",
+                };
+            }
+
+            return new Response
+            {
+                IsSuccess = true,
+                Message= "OK",
+            };
+        }
+
+        public async Task<TokenResponse> GetToken(string urlBase, string username, string password)
+        {
+            try
+            {
+                var client = new HttpClient();
+                client.BaseAddress = new Uri(urlBase);
+
+                var response = await client.PostAsync("Token",
+                    new StringContent(String.Format("grant_type=password&username={0}&password={1}",username,password),
+                    Encoding.UTF8,"application/x-www-form-urlencoded"));
+
+                var resultJSON = await response.Content.ReadAsStringAsync();
+
+                var result = JsonConvert.DeserializeObject<TokenResponse>(resultJSON);
+
+                return result;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
