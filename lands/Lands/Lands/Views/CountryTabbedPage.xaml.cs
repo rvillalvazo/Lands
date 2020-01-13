bba2ce8c4cb027cd35be4ef4bcf9f21d4c254f4a
rvillalvazo <59748238+rvillalvazo@users.noreply.github.com>
2020-01-13 11:25:40 -0600

    Create CountryTabbedPage.xaml.cs

---
+++ lands/Lands/Lands/Views/CountryTabbedPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿namespace Lands.Views
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    using Xamarin.Forms;
+    using Xamarin.Forms.Xaml;
+
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class CountryTabbedPage : TabbedPage
+	{
+		public CountryTabbedPage ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
