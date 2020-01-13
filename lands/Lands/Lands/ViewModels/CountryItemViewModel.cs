6b056f797501a57c0d23ce1480859223ecb20e29
rvillalvazo <59748238+rvillalvazo@users.noreply.github.com>
2020-01-13 00:41:21 -0600

    Create CountryItemViewModel.cs

---
+++ lands/Lands/Lands/ViewModels/CountryItemViewModel.cs
@@ -0,0 +1,31 @@
+﻿
+namespace Lands.ViewModels
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using Models;
+    using GalaSoft.MvvmLight.Command;
+    using System.Windows.Input;
+    using Xamarin.Forms;
+    using Views;
+
+    public class CountryItemViewModel:Country
+    {
+        #region Comandos
+        public ICommand SelectLandCommand
+        {
+            get
+            {
+                return new RelayCommand(SelectLand);
+            }
+        }
+
+        public async void SelectLand()
+        {
+            MainViewModel.GetInstance().Country = new CountryViewModel(this);
+            await Application.Current.MainPage.Navigation.PushAsync(new CountryPage());
+        }
+        #endregion
+    }
+}
