09f6fd8a71a443d85eb75e1d3e2158faf2c11d41
rvillalvazo <59748238+rvillalvazo@users.noreply.github.com>
2020-01-13 00:41:25 -0600

    Create CountryViewModel.cs

---
+++ lands/Lands/Lands/ViewModels/CountryViewModel.cs
@@ -0,0 +1,28 @@
+﻿namespace Lands.ViewModels
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using Models;
+
+    public class CountryViewModel
+    {
+        #region Variables
+        #endregion
+
+        #region Propiedades
+        public CountryItemViewModel Country
+        {
+            get;
+            set;
+        }
+        #endregion
+
+        #region Constructor
+        public CountryViewModel(CountryItemViewModel country)
+        {
+            this.Country = country;
+        }
+        #endregion
+    }
+}
