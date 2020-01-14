f5a47701b2dac63ce384f16e48a4fd1d6764b9c1
rvillalvazo <59748238+rvillalvazo@users.noreply.github.com>
2020-01-13 22:20:43 -0600

    Create Border.cs

---
+++ lands/Lands/Lands/Models/Border.cs
@@ -0,0 +1,19 @@
+﻿namespace Lands.Models
+{
+    public class Border
+    {
+        #region Propiedades
+        public string Code
+        {
+            get;
+            set;
+        }
+
+        public string Name
+        {
+            get;
+            set;
+        }
+        #endregion
+    }
+}
