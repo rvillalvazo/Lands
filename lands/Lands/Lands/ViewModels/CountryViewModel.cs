e08dc9012abda414873c5d53acdb41931e9ba514
rvillalvazo <59748238+rvillalvazo@users.noreply.github.com>
2020-01-13 22:20:47 -0600

    Update CountryViewModel.cs

--- lands/Lands/Lands/ViewModels/CountryViewModel.cs
+++ lands/Lands/Lands/ViewModels/CountryViewModel.cs
@@ -9,11 +9,27 @@
 
     public class CountryViewModel:BaseViewModel
     {
+        #region Variables
+        private ObservableCollection<Border> _borders = new ObservableCollection<Border>();
+        #endregion
+
         #region Propiedades
         public CountryItemViewModel Country
         {
             get;
             set;
+        }
+
+        public ObservableCollection<Border> Borders
+        {
+            get
+            {
+                return this._borders;
+            }
+            set
+            {
+                SetValue(ref this._borders,value);
+            }
         }
         #endregion
 
@@ -21,6 +37,27 @@
         public CountryViewModel(CountryItemViewModel country)
         {
             this.Country = country;
+            this.LoadBorders();
+        }
+        #endregion
+
+        #region Metodos
+        private void LoadBorders()
+        {
+            this.Borders = new ObservableCollection<Border>();
+            foreach(var border in this.Country.Borders)
+            {
+                var country = MainViewModel.GetInstance().CountriesList.Where(c => c.Alpha3Code.Equals(border)).FirstOrDefault();
+
+                if(country!=null)
+                {
+                    this._borders.Add(new Border
+                                            {
+                                                Code =country.Alpha3Code,
+                                                Name =country.Name
+                                             });
+                }
+            }
         }
         #endregion
     }
